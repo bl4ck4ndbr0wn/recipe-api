3ab8018bf15d89f53be5bbfe723888e57b397772
bl4ck4ndbr0wn <alphandirangu@gmail.com>
2019-03-10 22:12:10 +0300

    Resolved indentation ussues.

--- app/core/models.py
+++ app/core/models.py
@@ -5,33 +5,33 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 
 class UserManager(BaseUserManager):
 
-  def create_user(self, email, password=None, **extra_fields):
-    """Creates and saves anew user"""
-    if not email:
-      raise ValueError('User must have an email address')
-    user = self.model(email=self.normalize_email(email), **extra_fields)
-    user.set_password(password)
-    user.save(using=self.db)
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves anew user"""
+        if not email:
+            raise ValueError('User must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
 
-    return user
+        return user
 
-  def create_superuser(self, email, password):
-    """Creates and saves a new supper user"""
-    user = self.create_user(email, password)
-    user.is_staff = True
-    user.is_superuser = True
-    user.save(using=self._db)
+    def create_superuser(self, email, password):
+        """Creates and saves a new supper user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
-    return user
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-  """Custom user model that supports using email instead of username"""
-  email  = models.EmailField(max_length=254, unique=True)
-  name = models.CharField(max_length=255)
-  is_active = models.BooleanField(default=True)
-  is_staff = models.BooleanField(default=False)
+    """Custom user model that supports using email instead of username"""
+    email  = models.EmailField(max_length=254, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-  objects = UserManager()
+    objects = UserManager()
 
-  USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'email'
