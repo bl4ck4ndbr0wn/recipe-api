17bfc3a2bb94c0cf7c679bd30f9b166611a5dbbc
bl4ck4ndbr0wn <alphandirangu@gmail.com>
2019-03-22 10:47:01 +0300

    Renamed swagger to diplay app name

--- app/urls.py
+++ app/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls import url
 
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title='Pastebin API')
+schema_view = get_swagger_view(title='Recipe API')
 
 
 urlpatterns = [
